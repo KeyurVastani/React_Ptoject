48d3ffd4d0a04447d8382f8919ed2ebdf9217f11
John Doe <johndoe@example.com>
2021-08-14 07:28:50 +0530

    first commit

---
+++ Demo1/App.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+
+
+// or any pure javascript modules available in npm
+
+
+const  App=()=> {
+  return (
+    <View >
+      <Text >
+       This is my project
+      </Text>
+      
+    </View>
+  );
+}
+
+
+
+
+export default App
